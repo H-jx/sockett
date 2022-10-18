d1ccf22b7f56348b8f2b5ee913e912cc06ede176
hb-bobo <751968840@qq.com>
2022-10-19 06:27:38 +0800

    feat: Browser support

--- src/Sockett.ts
+++ src/Sockett.ts
@@ -1,25 +1,25 @@
 import { EventEmitter } from "events";
-import WebSocket from "ws";
-import { isOpen } from "./utils";
+import WebSocketServer from "ws";
 
-export interface SockettOptions {
+export interface SockettOptions extends WebSocketServer.ClientOptions {
     protocols?: string | string[];
     timeout?: number;
     maxAttempts?: number;
 }
 interface EventMap {
-    open: (ev: WebSocket.OpenEvent) => any;
-    message: (ev: WebSocket.MessageEvent) => any;
-    reconnect: (ev: WebSocket.OpenEvent | WebSocket.CloseEvent) => any;
-    maximum: (ev: WebSocket.CloseEvent) => any;
-    close: (ev: WebSocket.CloseEvent) => any;
-    error: (ev: WebSocket.ErrorEvent) => any;
+    open: (ev: Event) => WebSocket;
+    message: (ev: MessageEvent) => void;
+    reconnect: (ev: ErrorEvent) => void;
+    maximum: (ev: CloseEvent) => void;
+    close: (ev: CloseEvent) => void;
+    error: (ev: ErrorEvent) => void;
 }
 
 const CLOSE_CODE = 1e3;
 const defaultOptions = {
     timeout: 1e3
 };
+
 export class Sockett extends EventEmitter {
     public wss!: WebSocket;
     public num: number;
@@ -30,14 +30,15 @@ export class Sockett extends EventEmitter {
     /**
      * 缓存opened之前调用的消息
      */
-    public messageCache: string[];
+    public messageCache: (ArrayBufferLike | string)[];
     constructor(url: string, opts: SockettOptions = {}) {
         super();
         this.url = url;
+        this.max = opts.maxAttempts === undefined ? Infinity : opts.maxAttempts;
+        delete opts.maxAttempts;
         this.opts = Object.assign({}, defaultOptions, opts);
         this.num = 0;
         this.timer = 1;
-        this.max = opts.maxAttempts === undefined ? Infinity : opts.maxAttempts;
         this.messageCache = [];
         this.open();
     }
@@ -46,7 +47,7 @@ export class Sockett extends EventEmitter {
      */
     public emit = <T extends keyof EventMap>(
         event: T,
-        arg: Parameters<EventMap[T]>[0]
+        ...arg: Parameters<EventMap[T]>
     ) => {
         return super.emit(event, arg);
     };
@@ -55,13 +56,17 @@ export class Sockett extends EventEmitter {
      */
     public on = <E extends keyof EventMap>(
         event: E,
-        listener: (arg: Parameters<EventMap[E]>[0]) => void
+        listener: (...arg: Parameters<EventMap[E]>) => void
     ) => {
         return super.on(event, listener);
     };
     public open = () => {
-        this.wss = new WebSocket(this.url, this.opts.protocols || []);
-
+        if ('WebSocket' in window) {
+            this.wss = new WebSocket(this.url, this.opts.protocols || []);
+        } else {
+            this.wss = new WebSocketServer(this.url, this.opts.protocols || [], this.opts) as any;
+        }
+    
         this.wss.onmessage = e => {
             this.emit("message", e);
         };
@@ -84,7 +89,7 @@ export class Sockett extends EventEmitter {
         this.wss.onerror = e => {
             e && e.type === "ECONNREFUSED"
                 ? this.reconnect(e)
-                : this.emit("error", e);
+                : this.emit("error", e as ErrorEvent);
         };
     };
 
@@ -103,33 +108,30 @@ export class Sockett extends EventEmitter {
         this.send(JSON.stringify(message));
     };
 
-    public send = (message: any) => {
+    public send = (message: string | ArrayBufferLike) => {
         // opening
         if (this.wss.readyState === this.wss.CONNECTING && !this.messageCache.includes(message)) {
             this.messageCache.push(message);
             return;
         }
         if (!this.isOpen()) {
-            this.emit("error", {
-                error: "error",
+            this.emit("error", new ErrorEvent("error", {
                 message: "ws is not opening",
-                type: "error",
-                target: this.wss
-            });
+            }));
             return;
         }
         this.wss.send(message);
     };
 
-    public close = (code = CLOSE_CODE, data?: any) => {
+    public close = (code = CLOSE_CODE, reason?: string) => {
         if (!this.isOpen()) {
             return;
         }
-        clearTimeout(this.timer as NodeJS.Timeout);
+        clearTimeout(this.timer);
         this.timer = -1;
-        this.wss.close(code, data);
+        this.wss.close(code, reason);
     };
     public isOpen() {
-        return this.wss && isOpen(this.wss);
+        return this.wss && this.wss.readyState === (WebSocket || WebSocketServer).OPEN;
     }
 }
